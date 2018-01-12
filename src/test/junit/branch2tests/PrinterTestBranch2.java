afc2bd1b0072b143196da1c525345a7bdb296fd6
Jendoliver <alvarezmonllor@gmail.com>
2018-01-12 20:15:07 +0100

    vas a pasar hijodeputaaaaaa

---
+++ src/test/junit/branch2tests/PrinterTestBranch2.java
@@ -0,0 +1,19 @@
+package junit.branch2tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PrinterTestBranch2 
+{
+	@Test
+	public void failedTest() throws Exception
+	{
+		assertEquals("Me puedes mostrar algun mensaje hijo de puta?", "SORRYMAN", "GONNAFAILT");
+	}
+	
+	@Test
+	public void chickyTest() throws Exception
+	{
+		assertEquals("Sick", "NOICE", "NOICE");
+	}
+}
